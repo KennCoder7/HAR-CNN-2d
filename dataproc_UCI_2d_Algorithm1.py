56768bd572b55818ff7a3b8c2bf420f1a3d8d78c
KennCoder7 <iskenn7@gmail.com>
2018-11-05 18:24:45 +0800

    change

--- dataproc_UCI_2d_Algorithm1.py
+++ dataproc_UCI_2d_Algorithm1.py
@@ -17,21 +17,14 @@ def read_array(segmt, n):
 
 
 def array_algorithm1(arr):
-    re_array_method1 = np.array([1, 3, 5, 7, 9, 2, 4, 6, 8], dtype=int)
-    re_array_method2 = np.array([1, 4, 7, 1, 5, 8, 2, 5, 9], dtype=int)
-    re_array_method3 = np.array([3, 6, 9, 4, 8, 3, 7, 2, 6], dtype=int)
-    # new_array = np.array([], dtype=float)
-    new_array1 = np.array([], dtype=float)
-    new_array2 = np.array([], dtype=float)
-    new_array3 = np.array([], dtype=float)
-    for i in range(len(arr)):
-        new_array1 = np.append(new_array1, arr[re_array_method1[i] - 1])
-        new_array2 = np.append(new_array2, arr[re_array_method2[i] - 1])
-        new_array3 = np.append(new_array3, arr[re_array_method3[i] - 1])
-    new_array = np.append(arr, new_array1)
-    new_array = np.append(new_array, new_array2)
-    new_array = np.append(new_array, new_array3)
-    return new_array    # 123456789 135792468 147158259 369483726
+    re_array_method = np.array([[1, 3, 5, 7, 9, 2, 4, 6, 8], [1, 4, 7, 1, 5, 8, 2, 5, 9], [3, 6, 9, 4, 8, 3, 7, 2, 6]],
+                               dtype=int)
+    new_array = np.array([], dtype=float)
+    for n in range(re_array_method.shape[0]):
+        for i in range(len(arr)):
+            new_array = np.append(new_array, arr[re_array_method[n][i] - 1])
+    out_array = np.append(arr, new_array)
+    return out_array  # 123456789 135792468 147158259 369483726
 
 
 def seg_algorithm1(data):
